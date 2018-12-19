7165e1c7630f9de71a668bb90676604e02b25f0c
Mauricio Allende <mauricio.allende.o@gmail.com>
2018-12-19 14:46:37 -0300

    add retryUntil and mayThrow helpers

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,4 +1,44 @@
 /**
+ * Throws an error message randomly given a probability number
+ * @param {number} prob Probability of failure
+ * @param {string} msg Error message
+ * @throws {error} msgOrErr
+ */
+export const mayThrow = (
+  prob = 0.1,
+  msgOrErr = 'There was an error'
+) => {
+  if (Math.random() < prob) {
+    throw msgOrErr
+  }
+}
+
+/**
+ * Run an async function until it doesn't throw
+ * @param {promise} promise Promise to run on each retry
+ * @param {number} retries Max number of retries before it throws
+ * @param {function} onError Callback executed on each error
+ * @throws {error} Error object coming from the failed promise
+ * @returns {*} Promise value
+ */
+export const retryUntil = async (promise, retries = Infinity, onError) => {
+  const retry = async (num = 0) => {
+    try {
+      return await promise()
+    } catch (err) {
+      if (num + 1 > retries) throw err
+      if (typeof onError !== 'undefined' && onError) {
+        onError(err)
+      }
+
+      retry(num + 1)
+    }
+  }
+
+  retry()
+}
+
+/**
  * Create a custom error
  * @param {string} name
  * @param {string} message
